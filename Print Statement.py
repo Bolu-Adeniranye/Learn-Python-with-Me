71777f7a8867c8491a7722346020b82f53e7ca9a
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2024-07-15 15:42:46 +0100

    Python

---
+++ Print Statement.py
@@ -0,0 +1,4 @@
+print('Welcome to Python 101!')
+print("create nails")
+print("Create hammer")
+print("Use hammer and nails") 
---
+++ Variables & Datatypes.py
@@ -0,0 +1,6 @@
+print('Variables & Datatypes - Exercise')
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+item_name = "Apple"
+item_price = 50.45
+no_of_items = 45
