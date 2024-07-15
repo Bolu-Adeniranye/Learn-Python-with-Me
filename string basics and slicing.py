e3a7dd3671e5dd2ce81ceb169e47184f75c931e2
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2024-07-15 15:43:06 +0100

    Python

--- string basics and slicing.py
+++ string basics and slicing.py
@@ -2,5 +2,5 @@ msg='welcome to Python 101: Strings'
 msg1 = msg[0:7] + ' ' + msg[25] + msg[26:29] + ' ' + msg[24] + msg[4] + ' ' + msg[8] + msg[12] + msg[2] + msg[1] + msg[25]
 print(msg)
 print(msg1)
+print (msg1[::-1])
 print(msg1.title())
-print (msg1[::-2])
